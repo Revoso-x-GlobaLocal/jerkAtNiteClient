bf04e5d480717e194fc16999c21b48e10595cb54
Simeon Meerson <sm2806@georgetown.mei.georgetown.edu>
2020-10-06 16:33:04 -0400

    clearing merges

---
+++ lib/features/login/presentation/pages/login_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:jerk_at_nite_client/features/login/presentation/widgets/login_form.dart';
+
+
+
+
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(200),
+          child: LoginForm(),
+        ),
+      ],
+    ));
+  }
+}
