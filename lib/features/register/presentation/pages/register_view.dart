028810d503f32795df65164f8eef0ce57f968ee8
Jhamere Wilson <jhamerewilson@MacBook-Pro-3.local>
2020-10-06 18:55:26 -0400

    sign up/ login buttons created

--- lib/features/register/presentation/pages/register_view.dart
+++ lib/features/register/presentation/pages/register_view.dart
@@ -15,31 +15,20 @@ class RegisterScreen extends StatelessWidget {
       backgroundColor: Color(0xFFf9f9f9),
       body: Column(
         children: [
-          Material(
-            elevation: 1.5,
-            child: Container(
-              height: screenWidth * 0.035,
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              decoration: BoxDecoration(boxShadow: <BoxShadow>[]),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Jerk at Nite",
-                    style: TextStyle(
-                        color: Colors.red[900],
-                        fontSize: 24,
-                        fontWeight: FontWeight.w300),
-                  ),
-                  Text(
-                    "Login/Register",
-                    style: TextStyle(
-                        color: Colors.red[900],
-                        fontSize: 16,
-                        fontWeight: FontWeight.w100),
-                  )
-                ],
-              ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(),
+                IconButton(
+                    icon: Icon(
+                      Icons.highlight_remove_outlined,
+                      color: Colors.green[900],
+                      size: 36,
+                    ),
+                    onPressed: () {})
+              ],
             ),
           ),
           Expanded(
@@ -49,54 +38,59 @@ class RegisterScreen extends StatelessWidget {
                 children: [
                   SizedBox(height: screenHeight * .1),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Text(
-                      "Do Ya Body Right!",
+                      "Create An Account.",
                       style: TextStyle(
-                          color: Color(0xFF2A4D14),
+                          color: Colors.green[900],
                           fontSize: 48,
                           fontWeight: FontWeight.w900),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Create a Jerk at Nite \nCustomer Account.",
-                      style: TextStyle(
-                          color: Color(0xFF317B22),
-                          fontSize: 48,
-                          fontWeight: FontWeight.w900),
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already a member?",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w100,
+                            fontSize: 18),
+                      ),
+                      FlatButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                                color: Colors.green[900],
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18),
+                          ))
+                    ],
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "All fields with an * are required",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w100),
-                    ),
-                  ),
-                  SizedBox(height: screenHeight * .1),
+                  SizedBox(height: screenHeight * .075),
                   Container(
                     // padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.15),
                     child: RegisterForm(),
                   ),
                   Container(
-                    height: screenHeight * 0.025,
+                    height: screenHeight * 0.055,
                     width: screenWidth * 0.025,
                     padding: EdgeInsets.symmetric(horizontal: screenWidth * .1),
                     child: FlatButton(
-                      color: Color(0xFF2A4D14),
-                      disabledColor: Colors.white10,
-                        onPressed: () {}, child: Text("Create Account", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w100),)),
-                  ),
-                  Container(
-                    height: screenHeight * 0.1,
-                    child: Column(
-                      children: [],
-                    ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            side:
+                                BorderSide(color: Colors.green[900], width: 2)),
+                        color: Colors.transparent,
+                        disabledColor: Colors.white10,
+                        onPressed: () {},
+                        child: Text(
+                          "Create Account",
+                          style: TextStyle(
+                              color: Colors.green[900],
+                              fontWeight: FontWeight.w900),
+                        )),
                   ),
                 ],
               ),
