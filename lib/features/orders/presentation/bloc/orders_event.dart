a2f599617c5403f7af8fac3f00553f128d99c17c
Jhamere Wilson <jhamerewilson@MacBook-Pro-3.local>
2020-10-06 13:59:12 -0400

    auth and orders features created

---
+++ lib/features/orders/presentation/bloc/orders_event.dart
@@ -0,0 +1,3 @@
+part of 'orders_bloc.dart';
+@immutable
+abstract class OrdersEvent {}
---
+++ lib/features/orders/presentation/bloc/orders_state.dart
@@ -0,0 +1,4 @@
+part of 'orders_bloc.dart';
+@immutable
+abstract class OrdersState {}
+class OrdersInitial extends OrdersState {}
